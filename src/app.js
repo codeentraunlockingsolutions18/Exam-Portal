768c3bf2810d0f7d09ac12b68a2869e4df73db48
priyam karn <01fe20bcs299@kletech.ac.in>
2025-05-25 11:52:02 +0530

    corse resolved

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
-// import cors from "cors";
+import cors from "cors"; // ✅ UNCOMMENT this
+
 const app = express();
 
 dotenv.config({
@@ -13,11 +14,17 @@ import sequelize from "./db/index.js";
 // import models
 import Course from "./models/courses.model.js";
 
-//routes import
-
+// routes import
 import userRouter from "./routes/user.route.js";
 import allCourses from "./routes/course.route.js";
 
+// ✅ Add CORS middleware here
+app.use(cors({
+  origin: "http://localhost:8080",
+  credentials: true
+}));
+
+// middleware
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
@@ -31,7 +38,6 @@ app.get("/", (req, res) => {
 
 sequelize
   .sync()
-  //   .sync({ force: true })
   .then(() => {
     app.on("error", (error) => {
       console.log("Error Event For App !!", error);
