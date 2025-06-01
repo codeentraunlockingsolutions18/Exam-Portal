d7c31ec21abd3b55629b2efcb73acd0774bb77c8
Belal <mohammadvns121@gmail.com>
2025-06-02 01:25:26 +0530

    update delete fetch single quiezz comleted

--- backend/src/routes/quizzes.route.js
+++ backend/src/routes/quizzes.route.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import {
   createQuizze,
-  coursesQuizzes,
+  deleteQuizze,
+  getQuizze,
+  updateQuizze,
+  coursesQuizzes
 } from "../controllers/quizzes.controller.js";
+
+
 
 const router = Router();
 router.route("/quizze").post(createQuizze);
+router.route("/quizze/:queizz_id").get(getQuizze);
+router.route("/delete/:queizz_id").delete(deleteQuizze);
+router.route("/update/:queizz_id").put(updateQuizze);
 router.route("/quizzes").get(coursesQuizzes);
+
 
 export default router;
